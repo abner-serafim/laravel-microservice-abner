7b1f572ebc095fe7aff9ef2277fb8b8260d0a01e
Abner Serafim Arede <link.serafim@gmail.com>
2021-02-14 01:25:44 -0300

    Join with Genre And Category

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Video;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class VideoController extends BasicCrudController
 {
@@ -17,7 +19,31 @@ class VideoController extends BasicCrudController
             'opened' => 'boolean',
             'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
             'duration' => 'required|integer',
+            'categories_id' => 'required|array|exists:categories,id',
+            'genres_id' => 'required|array|exists:genres,id'
         ];
+    }
+
+    public function store(Request $request): Model
+    {
+        $validationData = $this->validate($request, $this->getRulesStore());
+        /** @var Video $obj */
+        $obj = $this->getModel()::create($validationData);
+        $obj->categories()->sync($request->get('categories_id'));
+        $obj->genres()->sync($request->get('genres_id'));
+        $obj->refresh();
+        return $obj;
+    }
+
+    public function update(Request $request, $id): Model
+    {
+        $validationData = $this->validate($request, $this->getRulesUpdate());
+        /** @var Video $obj */
+        $obj = $this->findOrFail($id);
+        $obj->update($validationData);
+        $obj->categories()->sync($request->get('categories_id'));
+        $obj->genres()->sync($request->get('genres_id'));
+        return $obj;
     }
 
     protected function getModel(): string
--- app/Models/Video.php
+++ app/Models/Video.php
@@ -36,4 +36,14 @@ class Video extends Model
     ];
 
     public $incrementing = false;
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class);
+    }
 }
